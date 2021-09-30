e28b22e58a96a81b9d55e307160c883504d02a49
Mitesh Kumar <miteshkumarca@gmail.com>
2021-09-30 14:16:56 -0700

    create stable_storage.json

--- build.sh
+++ build.sh
@@ -4,12 +4,13 @@ rm -rf ./bin
 mkdir ./bin
 
 golocation="/usr/local/go/bin/go"
-//golocation="go"
+# golocation="go"
 # golocation=$(which go) # for local
 echo $golocation
 
 $golocation build -o main ./src/main.go
 
+ls
 cp main ./bin/
 cp run.sh ./bin/
 cp stable_storage.json ./bin/
